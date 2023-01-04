c340e977aec82c8df6718bf3d881b877106d2150
jsakamoto <junichi.jp@gmail.com>
2023-01-04 21:18:38 +0900

    Implement updating iframe URL with arguments

---
+++ BlazingStory/Internals/Models/AsyncEventHandler.cs
@@ -0,0 +1,15 @@
+﻿namespace BlazingStory.Internals.Models;
+
+internal delegate ValueTask AsyncEventHandler();
+
+internal static class AsyncEventHandlerExtensions
+{
+    public static async ValueTask InvokeAsync(this AsyncEventHandler? handler)
+    {
+        if (handler == null) return;
+        foreach (var invocation in handler.GetInvocationList())
+        {
+            await (ValueTask)(invocation.Method.Invoke(invocation.Target, Array.Empty<object>()) ?? ValueTask.CompletedTask);
+        }
+    }
+}
--- BlazingStory/Types/StoryContext.cs
+++ BlazingStory/Types/StoryContext.cs
@@ -4,12 +4,31 @@ namespace BlazingStory.Types;
 
 public class StoryContext
 {
-    public Dictionary<string, object?> Args { get; } = new();
+    private readonly Dictionary<string, object?> _Args = new();
+
+    public IReadOnlyDictionary<string, object?> Args => this._Args;
 
     internal readonly IEnumerable<ComponentParameter> Parameters;
+
+    internal event AsyncEventHandler? ArgumentChanged;
 
     internal StoryContext(IEnumerable<ComponentParameter> parameters)
     {
         this.Parameters = parameters;
+    }
+
+    internal async ValueTask AddOrUpdateArgumentAsync(string name, object? newValue)
+    {
+        if (this._Args.TryGetValue(name, out var value))
+        {
+            if ((value == null && newValue == null) || (value != null && value.Equals(newValue))) return;
+            this._Args[name] = newValue;
+        }
+        else
+        {
+            this._Args.Add(name, newValue);
+        }
+
+        await this.ArgumentChanged.InvokeAsync();
     }
 }
