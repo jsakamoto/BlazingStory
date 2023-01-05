8596ee7f016e0396bb1649b3e3a1e1bee654bee4
jsakamoto <junichi.jp@gmail.com>
2023-01-05 22:17:48 +0900

    Implement applying input args

---
+++ BlazingStory/Internals/Extensions/ValueTaskExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace BlazingStory.Internals.Extensions;
+
+internal static class ValueTaskExtensions
+{
+    public static void AndLogException(this ValueTask task, ILogger logger)
+    {
+        var awaiter = task.GetAwaiter();
+        awaiter.OnCompleted(() =>
+        {
+            if (!task.IsFaulted) return;
+            try { awaiter.GetResult(); }
+            catch (Exception e) { logger.LogError(e, message: e.Message); }
+        });
+    }
+}
