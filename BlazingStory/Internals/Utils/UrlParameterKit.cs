2e032ceb79e01e9ddafcf3b7c4eeb8b770ff49e1
jsakamoto <junichi.jp@gmail.com>
2023-02-01 08:34:37 +0900

    Improve handling uri query parameters

---
+++ BlazingStory/Internals/Utils/UrlParameterKit.cs
@@ -0,0 +1,31 @@
+﻿namespace BlazingStory.Internals.Utils;
+
+internal class UriParameterKit
+{
+    public static string EncodeKeyValues(IReadOnlyDictionary<string, object?>? keyValues)
+    {
+        if (keyValues == null) return "";
+        return string.Join(';', keyValues.Select(kv => Uri.EscapeDataString(kv.Key) + ":" + Uri.EscapeDataString(kv.Value?.ToString() ?? "")));
+    }
+
+    public static IReadOnlyDictionary<string, string> DecodeKeyValues(string? text)
+    {
+        return (text ?? "").Split(';')
+            .Where(chunk => !string.IsNullOrEmpty(chunk))
+            .Select(chunk => chunk.Split(':'))
+            .ToDictionary(
+                chunk => Uri.UnescapeDataString(chunk[0]),
+                chunk => Uri.UnescapeDataString(chunk[1]));
+    }
+
+    public static string GetUri(string uri, IReadOnlyDictionary<string, object?>? parameters)
+    {
+        if (parameters == null) return uri;
+#pragma warning disable SYSLIB0013 // Type or member is obsolete
+        var searchText = string.Join('&', parameters.Select(kv => Uri.EscapeUriString(kv.Key) + "=" + Uri.EscapeUriString(kv.Value?.ToString() ?? "")));
+#pragma warning restore SYSLIB0013 // Type or member is obsolete
+        if (string.IsNullOrEmpty(searchText)) return uri;
+
+        return uri + "?" + searchText;
+    }
+}
---
+++ Tests/BlazingStory.Test/Internals/Utils/UriParameterKitTest.cs
@@ -0,0 +1,70 @@
+﻿using System.Web;
+using BlazingStory.Internals.Utils;
+
+namespace BlazingStory.Test.Internals.Utils;
+
+internal class UriParameterKitTest
+{
+    [Test]
+    public void EncodeKeyValues_with_Null_Test()
+    {
+        UriParameterKit.EncodeKeyValues(null).Is("");
+    }
+
+    [Test]
+    public void EncodeKeyValues_Test()
+    {
+        UriParameterKit.EncodeKeyValues(new Dictionary<string, object?>
+        {
+            ["ABC"] = "DEF",
+            ["GH:I"] = "JKL",
+            ["MNO"] = "P:QR",
+            ["S;TU"] = "VW;X",
+        }).Is("ABC:DEF;GH%3AI:JKL;MNO:P%3AQR;S%3BTU:VW%3BX");
+    }
+
+    [Test]
+    public void DecodeKeyValues_with_Empty_Test()
+    {
+        UriParameterKit.DecodeKeyValues("").Count.Is(0);
+    }
+
+    [Test]
+    public void DecodeKeyValues_with_Null_Test()
+    {
+        UriParameterKit.DecodeKeyValues(null).Count.Is(0);
+    }
+
+    [Test]
+    public void DecodeKeyValues_Test()
+    {
+        var keyValues = UriParameterKit.DecodeKeyValues("ABC:DEF;GH%3AI:JKL;MNO:P%3AQR;S%3BTU:VW%3BX");
+        keyValues["ABC"].Is("DEF");
+        keyValues["GH:I"].Is("JKL");
+        keyValues["MNO"].Is("P:QR");
+        keyValues["S;TU"].Is("VW;X");
+    }
+
+    [Test]
+    public void GetUri_Test()
+    {
+        var uri = UriParameterKit.GetUri("./iframe.html", new Dictionary<string, object?>
+        {
+            ["viewMode"] = "story",
+            ["args"] = UriParameterKit.EncodeKeyValues(new Dictionary<string, object?>
+            {
+                ["Flag"] = true,
+                ["Text"] = "Date: 2023/02/01",
+                ["Width"] = 640
+            })
+        });
+        uri.Is("./iframe.html?viewMode=story&args=Flag:True;Text:Date%253A%25202023%252F02%252F01;Width:640");
+
+        var queryStrings = HttpUtility.ParseQueryString(new Uri("https://example.com/" + uri).Query);
+        queryStrings["viewMode"].Is("story");
+        var args = UriParameterKit.DecodeKeyValues(queryStrings["args"]);
+        args["Flag"].Is("True");
+        args["Text"].Is("Date: 2023/02/01");
+        args["Width"].Is("640");
+    }
+}
