846dd35393da06059f08d13c932e175f0759f29f
jsakamoto <junichi.jp@gmail.com>
2023-01-31 23:16:03 +0900

    Implementing grid preview

--- BlazingStory/Internals/Services/Command/CommandService.cs
+++ BlazingStory/Internals/Services/Command/CommandService.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections.Specialized;
-using BlazingStory.Internals.Extensions;
+﻿using BlazingStory.Internals.Extensions;
 using Microsoft.Extensions.Logging;
 using Toolbelt.Blazor.HotKeys2;
 
@@ -9,82 +8,50 @@ internal class CommandService : IDisposable
 {
     private readonly HotKeys _HotKeys;
 
-    private readonly HelperScript _HelperScript;
-
     private readonly HotKeysContext _HotKeysContext;
 
     private readonly ILogger<CommandService> _Logger;
 
-    private readonly OrderedDictionary _Commands = new();
+    private string CommandStateKeyName => this.GetType().Name + "." + nameof(this.Commands);
 
-    private string CommandStateKeyName => this.GetType().Name + "." + nameof(this._Commands);
-
-    private bool _Initialized = false;
-
-    public IEnumerable<(CommandType Type, Command Command)> Commands => this._Commands.Keys.Cast<CommandType>().Select(key => (key, this[key]!));
+    internal readonly CommandSet<CommandType> Commands;
 
     public CommandService(HotKeys hotKeys, HelperScript helperScript, ILogger<CommandService> logger)
     {
+        this.Commands = new CommandSet<CommandType>(this.CommandStateKeyName, helperScript, logger);
         this._HotKeys = hotKeys;
-        this._HelperScript = helperScript;
         this._Logger = logger;
         this._HotKeysContext = this._HotKeys.CreateContext();
         this._HotKeys.KeyDown += this.HotKeys_OnKeyDown;
     }
 
-    public async ValueTask EnsureCommandsAsync(Func<IEnumerable<(CommandType Type, Command Command)>> getCommandEntries)
-    {
-        if (this._Initialized) return;
-        this._Initialized = true;
-
-        var commandStates = await this._HelperScript.LoadObjectFromLocalStorageAsync(this.CommandStateKeyName, new Dictionary<CommandType, CommandState>());
-        foreach (var cmdEntry in getCommandEntries())
-        {
-            if (commandStates.TryGetValue(cmdEntry.Type, out var state)) state.Apply(cmdEntry.Command);
-            cmdEntry.Command.StateChanged += this.Command_StateChanged;
-            this._Commands.Add(cmdEntry.Type, cmdEntry.Command);
-        }
-    }
-
-    public Command? this[CommandType type] => this._Commands[(object)type] as Command;
+    public Command? this[CommandType type] => this.Commands[type];
 
     public async Task InvokeAsync(CommandType type)
     {
-        if (this._Commands[type] is not Command command) return;
+        if (this.Commands[type] is not Command command) return;
         await command.InvokeAsync();
     }
 
     public IDisposable Subscribe(CommandType type, AsyncCallback callBack)
     {
-        if (this._Commands[type] is not Command command) throw new KeyNotFoundException();
+        if (this.Commands[type] is not Command command) throw new KeyNotFoundException();
         return command.Subscribe(callBack);
     }
 
     private void HotKeys_OnKeyDown(object? sender, HotKeyDownEventArgs args)
     {
         if (args.SrcElementTagName is "TEXTAREA" or "INPUT") return;
-        var commad = this._Commands.Values.Cast<Command>()
+        var commad = this.Commands
+            .Select(entry => entry.Command)
             .FirstOrDefault(cmd => cmd.HotKey != null && cmd.HotKey.Code == args.Code && cmd.HotKey.Modifiers == args.Modifiers);
         if (commad == null) return;
         commad.InvokeAsync().AndLogException(this._Logger);
     }
 
-    private void Command_StateChanged(object? sender, EventArgs e)
-    {
-        var commandStates = this._Commands.Keys
-            .Cast<CommandType>()
-            .ToDictionary(key => key, key => new CommandState(this[key]!));
-        this._HelperScript
-            .SaveObjectToLocalStorageAsync(this.CommandStateKeyName, commandStates)
-            .AndLogException(this._Logger);
-    }
-
     public void Dispose()
     {
-        foreach (var cmd in this._Commands.Values.Cast<Command>())
-        {
-            cmd.StateChanged -= this.Command_StateChanged;
-        }
+        this.Commands.Dispose();
         this._HotKeys.KeyDown -= this.HotKeys_OnKeyDown;
         this._HotKeysContext.Dispose();
     }
---
+++ BlazingStory/Internals/Services/Command/CommandSet.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Specialized;
+using BlazingStory.Internals.Extensions;
+using Microsoft.Extensions.Logging;
+
+namespace BlazingStory.Internals.Services.Command;
+
+internal class CommandSet<TKey> : IDisposable, IEnumerable<(TKey Type, Command Command)>
+    where TKey : struct, Enum
+{
+    private readonly string _StorageKey;
+
+    private readonly HelperScript _HelperScript;
+
+    private readonly ILogger _Logger;
+
+    private readonly OrderedDictionary _Commands = new();
+
+    private bool _Initialized = false;
+
+    public Command? this[TKey type] => this._Commands[(object)type] as Command;
+
+    internal CommandSet(string storageKey, HelperScript helperScript, ILogger logger)
+    {
+        this._StorageKey = storageKey;
+        this._HelperScript = helperScript;
+        this._Logger = logger;
+    }
+
+    internal async ValueTask EnsureInitializedAsync(Func<IEnumerable<(TKey Type, Command Command)>> getCommandEntries)
+    {
+        if (this._Initialized) return;
+        this._Initialized = true;
+
+        var commandStates = await this._HelperScript.LoadObjectFromLocalStorageAsync(this._StorageKey, new Dictionary<TKey, CommandState>());
+        foreach (var cmdEntry in getCommandEntries())
+        {
+            if (commandStates.TryGetValue(cmdEntry.Type, out var state)) state.Apply(cmdEntry.Command);
+            cmdEntry.Command.StateChanged += this.Command_StateChanged;
+            this._Commands.Add(cmdEntry.Type, cmdEntry.Command);
+        }
+    }
+
+    private void Command_StateChanged(object? sender, EventArgs e)
+    {
+        var commandStates = this._Commands.Keys
+            .Cast<TKey>()
+            .ToDictionary(key => key, key => new CommandState(this[key]!));
+        this._HelperScript
+            .SaveObjectToLocalStorageAsync(this._StorageKey, commandStates)
+            .AndLogException(this._Logger);
+    }
+
+    IEnumerator IEnumerable.GetEnumerator() => this.GetEnumerator();
+
+    public IEnumerator<(TKey Type, Command Command)> GetEnumerator()
+    {
+        return this._Commands.Keys.Cast<TKey>().Select(key => (key, this[key]!)).GetEnumerator();
+    }
+
+    public void Dispose()
+    {
+        foreach (var cmd in this._Commands.Values.Cast<Command>())
+        {
+            cmd.StateChanged -= this.Command_StateChanged;
+        }
+    }
+}
