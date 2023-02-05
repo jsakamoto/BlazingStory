e79aeb30f4de96534a301704a7ac470a8351bdb9
jsakamoto <junichi.jp@gmail.com>
2023-02-06 08:20:18 +0900

    Implementing the preview background color feature

--- BlazingStory/Internals/Pages/Canvas/CanvasFrame.razor.ts
+++ BlazingStory/Internals/Pages/Canvas/CanvasFrame.razor.ts
@@ -4,15 +4,23 @@ type StyleDescriptor = {
     enable: boolean
 };
 
-export const ensurePreviewStyle = (styleDescripters: StyleDescriptor[]) => {
+export const ensurePreviewStyle = (background: string, styleDescripters: StyleDescriptor[]) => {
+    const doc = document;
+    const head = doc.head;
+    const bodyStyle = doc.body.style;
+
+    bodyStyle.transition = "background-color 0.3s";
+    setTimeout(() => { bodyStyle.backgroundColor = background; }, 10);
+
+
     for (const descripter of styleDescripters) {
-        const linkElement = document.head.querySelector(`link#${descripter.id}`);
+        const linkElement = head.querySelector(`link#${descripter.id}`);
         if (linkElement === null && descripter.enable) {
-            const newLinkElement = document.createElement("link");
+            const newLinkElement = doc.createElement("link");
             newLinkElement.id = descripter.id;
             newLinkElement.href = descripter.href;
             newLinkElement.rel = "stylesheet";
-            document.head.appendChild(newLinkElement);
+            head.appendChild(newLinkElement);
         }
         else if (linkElement !== null && !descripter.enable) {
             linkElement.remove();
