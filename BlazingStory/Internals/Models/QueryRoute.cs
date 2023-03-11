647db8871a157d29e71ea9bd7d54c7f11d7130cd
jsakamoto <junichi.jp@gmail.com>
2023-03-11 22:13:21 +0900

    Improve the query router feature

---
+++ BlazingStory/Internals/Models/QueryRoute.cs
@@ -0,0 +1,17 @@
+﻿namespace BlazingStory.Internals.Models;
+
+public class QueryRoute
+{
+    internal readonly string ViewName;
+
+    internal readonly string Parameter;
+
+    internal readonly Type ViewComponent;
+
+    internal QueryRoute(string viewName, string parameter, Type viewComponent)
+    {
+        this.ViewName = viewName;
+        this.Parameter = parameter;
+        this.ViewComponent = viewComponent;
+    }
+}
