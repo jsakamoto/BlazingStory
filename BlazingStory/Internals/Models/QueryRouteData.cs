8a30c9d284bfbc698bee6f7ca79a720ac634347a
jsakamoto <junichi.jp@gmail.com>
2023-04-08 17:25:23 +0900

    Improve the implementation of the QueryRouteData

--- BlazingStory/Internals/Models/QueryRouteData.cs
+++ BlazingStory/Internals/Models/QueryRouteData.cs
@@ -1,38 +1,71 @@
-﻿namespace BlazingStory.Internals.Models;
+﻿using System.Web;
 
+namespace BlazingStory.Internals.Models;
+
+/// <summary>
+/// Represents the structured route data of the "path" query string of a URL like "http://.../?path=/story/example-button--primary".
+/// </summary>
 public class QueryRouteData : IEquatable<QueryRouteData?>
 {
+    /// <summary>
+    /// Gets the "path" query string that is source of this <see cref="QueryRouteData"/> instance.<br/>
+    /// </summary>
+    internal readonly string Path;
+
+    /// <summary>
+    /// Gets the first segment of the "path" query string.<br/>
+    /// If a URL like "http://.../?path=/story/example-button--primary" is given, this field will return "story".
+    /// </summary>
     internal readonly string View;
 
+    /// <summary>
+    /// Gets the second segment of the "path" query string.<br/>
+    /// If a URL like "http://.../?path=/story/example-button--primary" is given, this field will return "example-button--primary".
+    /// </summary>
     internal readonly string Parameter;
 
+    /// <summary>
+    /// Gets whether the View is "story" or "docs".<br/>
+    /// If a URL like "http://.../?path=/story/example-button--primary" is given, this field will return true.<br/>
+    /// If a URL like "http://.../?path=/settings/about" is given, this field will return false.<br/>
+    /// </summary>
     internal readonly bool RouteToStoryOrDocs;
 
-    public QueryRouteData(string path)
+    /// <summary>
+    /// Initialize a new instance of <see cref="QueryRouteData"/> from an URL like "http://.../?path=/story/example-button--primary" and a name of query parameter.
+    /// </summary>
+    /// <param name="uri">An URL to create a query route data from.</param>
+    /// <param name="queryName">A query parameter name to create a query route data from.</param>
+    public QueryRouteData(Uri uri, string queryName)
     {
-        var segments = path.Trim('/').Split('/');
+        var queryStrings = HttpUtility.ParseQueryString(uri.Query);
+        var queryString = queryStrings[queryName]?.Trim('/') ?? "";
+        var segments = queryString.Trim('/').Split('/');
+
+        this.Path = "/" + queryString;
         this.View = segments.FirstOrDefault() ?? "";
         this.Parameter = string.Join('/', segments.Skip(1));
         this.RouteToStoryOrDocs = this.View is "story" or "docs";
     }
 
-    public QueryRouteData(string viewMode, string id)
+    /// <summary>
+    /// Initialize a new instance of <see cref="QueryRouteData"/> with arguments to specify each field directly.<br/>
+    /// The <see cref="Path"/> field will be created from the <paramref name="view"/> and <paramref name="parameter"/> arguments.
+    /// </summary>
+    /// <param name="view">An argument for the <see cref="View"/> field</param>
+    /// <param name="parameter">An argument for the <see cref="Parameter"/> field</param>
+    public QueryRouteData(string view, string parameter)
     {
-        this.View = viewMode;
-        this.Parameter = id;
+        this.Path = $"/{view}/{parameter}";
+        this.View = view;
+        this.Parameter = parameter;
     }
 
     public override bool Equals(object? obj) => this.Equals(obj as QueryRouteData);
 
-    public bool Equals(QueryRouteData? other)
-    {
-        return other is not null &&
-               this.View == other.View &&
-               this.Parameter == other.Parameter &&
-               this.RouteToStoryOrDocs == other.RouteToStoryOrDocs;
-    }
+    public bool Equals(QueryRouteData? other) => other is not null && this.Path == other.Path;
 
-    public override int GetHashCode() => HashCode.Combine(this.View, this.Parameter, this.RouteToStoryOrDocs);
+    public override int GetHashCode() => HashCode.Combine(this.Path);
 
     public static bool operator ==(QueryRouteData? left, QueryRouteData? right) => EqualityComparer<QueryRouteData>.Default.Equals(left, right);
 
---
+++ Tests/BlazingStory.Test/Internals/Models/QueryRouteDataTest.cs
@@ -0,0 +1,36 @@
+﻿using BlazingStory.Internals.Models;
+
+namespace BlazingStory.Test.Internals.Models;
+
+internal class QueryRouteDataTest
+{
+    [Test]
+    public void Ctor_From_StoryUri_Test()
+    {
+        var routeData = new QueryRouteData(new Uri("http://localhost/?path=/story/example/button--primary"), queryName: "path");
+        routeData.Path.Is("/story/example/button--primary");
+        routeData.View.Is("story");
+        routeData.Parameter.Is("example/button--primary");
+        routeData.RouteToStoryOrDocs.IsTrue();
+    }
+
+    [Test]
+    public void Ctor_From_SettingsUri_Test()
+    {
+        var routeData = new QueryRouteData(new Uri("http://localhost/?path=/settings/about"), queryName: "path");
+        routeData.Path.Is("/settings/about");
+        routeData.View.Is("settings");
+        routeData.Parameter.Is("about");
+        routeData.RouteToStoryOrDocs.IsFalse();
+    }
+
+    [Test]
+    public void Ctor_From_ViewAndParam_Test()
+    {
+        var routeData = new QueryRouteData("viewMode", "id");
+        routeData.Path.Is("/viewMode/id");
+        routeData.View.Is("viewMode");
+        routeData.Parameter.Is("id");
+        routeData.RouteToStoryOrDocs.IsFalse();
+    }
+}
