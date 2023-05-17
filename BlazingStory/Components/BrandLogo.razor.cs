b674a8e715f602498a2d8999d89052cdf4df5a4e
jsakamoto <junichi.jp@gmail.com>
2023-05-17 21:23:25 +0900

    Improvement: The branding logo area to be customizable

---
+++ BlazingStory/Components/BrandLogo.razor.cs
@@ -0,0 +1,8 @@
+﻿namespace BlazingStory.Components;
+
+/// <summary>
+/// The default implementation of the brand logo of the Blazing Story app.
+/// </summary>
+public partial class BrandLogo
+{
+}
