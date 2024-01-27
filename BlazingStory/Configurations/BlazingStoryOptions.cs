e4911e77e1ebf0afd6e3fd3b4ed4ac87ec6ec40c
jsakamoto <junichi.jp@gmail.com>
2024-01-27 10:00:50 +0900

    Make hot reloading an optional feature

---
+++ BlazingStory/Configurations/BlazingStoryOptions.cs
@@ -0,0 +1,12 @@
+﻿namespace BlazingStory.Configurations;
+
+/// <summary>
+/// The configuration options for BlazingStory.
+/// </summary>
+internal class BlazingStoryOptions
+{
+    /// <summary>
+    /// [Preview feature] Gets or sets whether to enable hot reloading. (default: false)
+    /// </summary>
+    public bool EnableHotReloading { get; set; }
+}
