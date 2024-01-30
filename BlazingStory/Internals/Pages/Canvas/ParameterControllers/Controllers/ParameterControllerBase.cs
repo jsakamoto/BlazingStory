931ece8afe8efcf3d274f650ce5f5634c7bb3e86
jsakamoto <junichi.jp@gmail.com>
2024-01-31 08:42:38 +0900

    Refactoring: split parameters controllers for each parameter type

---
+++ BlazingStory/Internals/Pages/Canvas/ParameterControllers/Controllers/ParameterControllerBase.cs
@@ -0,0 +1,25 @@
+﻿using BlazingStory.Internals.Models;
+using Microsoft.AspNetCore.Components;
+
+namespace BlazingStory.Internals.Pages.Canvas.ParameterControllers.Controllers;
+
+public class ParameterControllerBase : ComponentBase
+{
+    [Parameter, EditorRequired]
+    public string? Key { get; set; }
+
+    [Parameter, EditorRequired]
+    public ComponentParameter? Parameter { get; set; }
+
+    [Parameter, EditorRequired]
+    public object? Value { get; set; }
+
+    [Parameter]
+    public EventCallback<ParameterInputEventArgs> OnInput { get; set; }
+
+    protected async Task OnInputAsync(object? value)
+    {
+        if (this.Parameter == null) throw new NullReferenceException("Parameter is null.");
+        await this.OnInput.InvokeAsync(new ParameterInputEventArgs(value, this.Parameter));
+    }
+}
