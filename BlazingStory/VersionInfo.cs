0c3655965bc7701c78b4663e16fb2405eb2710fe
jsakamoto <junichi.jp@gmail.com>
2024-08-06 08:48:24 +0900

    Investigating it works on NET9

--- BlazingStory/VersionInfo.cs
+++ BlazingStory/VersionInfo.cs
@@ -1,8 +1,8 @@
 namespace BlazingStory;
 internal static class VersionInfo
 {
-    internal static string GetVersionText() => "1.0.0-preview.37";
-    internal static string GetEscapedVersionText() => "1.0.0-preview.37";
-    internal static string GetFormattedVersionText() => "1.0 Preview 37";
+    internal static string GetVersionText() => "1.0.0-preview.37.5";
+    internal static string GetEscapedVersionText() => "1.0.0-preview.37.5";
+    internal static string GetFormattedVersionText() => "1.0 Preview 37.5";
     internal static string GetBuildTimestamp() => "May 2024";
 }
