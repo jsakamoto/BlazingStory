406b60807017408c19745319b6c45bac5a23af43
jsakamoto <junichi.jp@gmail.com>
2024-08-07 22:16:32 +0900

    Added a test to reproduce the problem that the summary text for a generic type component can't be retrieved

--- Tests/BlazingStory.Test/Internals/Services/XmlDocCommentTest.cs
+++ Tests/BlazingStory.Test/Internals/Services/XmlDocCommentTest.cs
@@ -2,23 +2,43 @@
 using BlazingStory.Test._Fixtures;
 using Microsoft.Extensions.DependencyInjection;
 using RazorClassLib1.Components.Button;
+using RazorClassLib1.Components.TextInput;
 
 namespace BlazingStory.Test.Internals.Services;
 
 internal class XmlDocCommentTest
 {
+    private static TestHost CreateTestHost() => new(services =>
+    {
+        services.AddSingleton(_ => XmlDocCommentLoaderFromOutDir.CreateHttpClient());
+        services.AddSingleton<IXmlDocComment, XmlDocCommentForWasm>();
+    });
+
     [Test]
     public async Task GetSummaryOfProperty_Test()
     {
-        await using var host = new TestHost(services =>
-        {
-            services.AddSingleton(_ => XmlDocCommentLoaderFromOutDir.CreateHttpClient());
-            services.AddSingleton<IXmlDocComment, XmlDocCommentForWasm>();
-        });
-
+        // Given
+        await using var host = CreateTestHost();
         var xmlDocComment = host.Services.GetRequiredService<IXmlDocComment>();
+
+        // When
         var summary = await xmlDocComment.GetSummaryOfPropertyAsync(typeof(Button), nameof(Button.Text));
 
+        // Then
         summary.Value.Is("Set a text that is button caption.");
+    }
+
+    [Test]
+    public async Task GetSummaryOfGenericComponent_Test()
+    {
+        // Given
+        await using var host = CreateTestHost();
+        var xmlDocComment = host.Services.GetRequiredService<IXmlDocComment>();
+
+        // When
+        var summary = await xmlDocComment.GetSummaryOfTypeAsync(typeof(TextInputBase<string>));
+
+        // Then
+        summary.Value.Is("A base class for text input components.");
     }
 }
