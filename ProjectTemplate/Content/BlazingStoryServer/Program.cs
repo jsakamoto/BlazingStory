9976e033b26977deda1e93278ed62de0b2d69700
jsakamoto <junichi.jp@gmail.com>
2025-01-23 22:13:01 +0900

    Update the project templates

--- ProjectTemplate/Content/BlazingStoryServer/Program.cs
+++ ProjectTemplate/Content/BlazingStoryServer/Program.cs
@@ -17,13 +17,21 @@ var app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
+#if (!NoHttps)
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+#endif
 }
 
+#if (!NoHttps)
 app.UseHttpsRedirection();
 
+#endif
+#if (Framework == "net8.0")
 app.UseStaticFiles();
+#else
+app.MapStaticAssets();
+#endif
 app.UseRouting();
 app.UseAntiforgery();
 
