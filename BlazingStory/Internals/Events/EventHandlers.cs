321567a2511bac5ed80613f7dfbb91e05208fbfd
jsakamoto <junichi.jp@gmail.com>
2025-02-15 11:52:34 +0900

    Enabled the "@onanimationend" event handler in Blazor

---
+++ BlazingStory/Internals/Events/EventHandlers.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel;
+using Microsoft.AspNetCore.Components;
+
+namespace BlazingStory.Internals.Events;
+
+[EditorBrowsable(EditorBrowsableState.Never)]
+[EventHandler("onanimationend", typeof(EventArgs), enableStopPropagation: true, enablePreventDefault: false)]
+public class EventHandlers
+{
+}
