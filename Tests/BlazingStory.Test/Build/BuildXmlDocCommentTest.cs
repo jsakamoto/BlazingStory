85651e1dbd66d333717d803b2062461b8aa8e5a2
jsakamoto <junichi.jp@gmail.com>
2025-03-28 16:05:25 -0700

    Update the test to reproduce issue #61

--- Tests/BlazingStory.Test/Build/BuildXmlDocCommentTest.cs
+++ Tests/BlazingStory.Test/Build/BuildXmlDocCommentTest.cs
@@ -10,7 +10,7 @@ internal class BuildXmlDocCommentTest
     [Parallelizable(ParallelScope.Self)]
     [TestCase("net8.0", "8.0.0", false)]
     [TestCase("net9.0", "9.0.0", false)]
-    public async Task DotNetRun_Test(string targetFramework, string SDKVersion, bool allowPrerelease)
+    public async Task BlazorWasm_DotNetRun_Test(string targetFramework, string SDKVersion, bool allowPrerelease)
     {
         // Given
         var testFixtureSpace = new TestFixtureSpace();
@@ -35,13 +35,16 @@ internal class BuildXmlDocCommentTest
         var xdocCommentForLib = XDocument.Load(uri: $"{listenUrl}/_framework/RazorClassLib1.xml");
         (xdocCommentForLib.Element("doc")?.Element("assembly")?.Element("name")?.Value).Is("RazorClassLib1");
 
+        var xdocCommentForFramework = XDocument.Load(uri: $"{listenUrl}/_framework/Microsoft.AspNetCore.Components.Web.xml");
+        (xdocCommentForFramework.Element("doc")?.Element("assembly")?.Element("name")?.Value).Is("Microsoft.AspNetCore.Components.Web");
+
         dotnetRun.Process.Kill(entireProcessTree: true);
     }
 
     [Parallelizable(ParallelScope.Self)]
     [TestCase("net8.0", "8.0.0", false)]
     [TestCase("net9.0", "9.0.0", false)]
-    public async Task Publish_Test(string targetFramework, string SDKVersion, bool allowPrerelease)
+    public async Task BlazorWasm_Publish_Test(string targetFramework, string SDKVersion, bool allowPrerelease)
     {
         // Given
         var testFixtureSpace = new TestFixtureSpace();
@@ -65,6 +68,76 @@ internal class BuildXmlDocCommentTest
 
         var xdocCommentForLib = XDocument.Load(uri: Path.Combine(frameworkDir, "RazorClassLib1.xml"));
         (xdocCommentForLib.Element("doc")?.Element("assembly")?.Element("name")?.Value).Is("RazorClassLib1");
+
+        var xdocCommentForFramework = XDocument.Load(uri: Path.Combine(frameworkDir, "Microsoft.AspNetCore.Components.Web.xml"));
+        (xdocCommentForFramework.Element("doc")?.Element("assembly")?.Element("name")?.Value).Is("Microsoft.AspNetCore.Components.Web");
+    }
+
+    [Parallelizable(ParallelScope.Self)]
+    [TestCase("net8.0", "8.0.0", false)]
+    [TestCase("net9.0", "9.0.0", false)]
+    public async Task BlazorServer_DotNetRun_Test(string targetFramework, string SDKVersion, bool allowPrerelease)
+    {
+        // Given
+        var testFixtureSpace = new TestFixtureSpace();
+        testFixtureSpace.CreateGlobalJson(SDKVersion, "latestMinor", allowPrerelease);
+        var emptyBlazorServerApp1Dir = testFixtureSpace.GetTestAppProjDir("EmptyBlazorServerApp1");
+        CsprojTools.Rewrite(emptyBlazorServerApp1Dir, targetFramework, testFixtureSpace.BlazingStoryTargetsPath);
+
+        var dotnetVersion = await XProcess.Start("dotnet", "--version", emptyBlazorServerApp1Dir, TestHelper.XProcessOptions).WaitForExitAsync();
+        dotnetVersion.ExitCode.Is(0, message: dotnetVersion.Output);
+        dotnetVersion.Output.Is(ver => ver.StartsWith(SDKVersion.Substring(0, 4)), message: dotnetVersion.Output);
+
+        // When
+        var listenUrl = $"http://localhost:{TestHelper.GetAvailableTCPv4Port()}";
+        using var dotnetRun = XProcess.Start("dotnet", $"run --urls {listenUrl}", emptyBlazorServerApp1Dir, TestHelper.XProcessOptions);
+        var success = await dotnetRun.WaitForOutputAsync(output => output.TrimStart().StartsWith("Now listening on: http"), millsecondsTimeout: 30000);
+        success.IsTrue(message: dotnetRun.Output);
+
+        // Then
+        var outputDir = Path.Combine(emptyBlazorServerApp1Dir, "bin", "Debug", targetFramework);
+        var xdocCommentForApp = XDocument.Load(uri: Path.Combine(outputDir, "EmptyBlazorServerApp1.xml"));
+        (xdocCommentForApp.Element("doc")?.Element("assembly")?.Element("name")?.Value).Is("EmptyBlazorServerApp1");
+
+        var xdocCommentForLib = XDocument.Load(uri: Path.Combine(outputDir, "RazorClassLib1.xml"));
+        (xdocCommentForLib.Element("doc")?.Element("assembly")?.Element("name")?.Value).Is("RazorClassLib1");
+
+        var xdocCommentForFramework = XDocument.Load(uri: Path.Combine(outputDir, "Microsoft.AspNetCore.Components.Web.xml"));
+        (xdocCommentForFramework.Element("doc")?.Element("assembly")?.Element("name")?.Value).Is("Microsoft.AspNetCore.Components.Web");
+
+        dotnetRun.Process.Kill(entireProcessTree: true);
+    }
+
+    [Parallelizable(ParallelScope.Self)]
+    [TestCase("net8.0", "8.0.0", false)]
+    [TestCase("net9.0", "9.0.0", false)]
+    public async Task BlazorServer_Publish_Test(string targetFramework, string SDKVersion, bool allowPrerelease)
+    {
+        // Given
+        var testFixtureSpace = new TestFixtureSpace();
+        testFixtureSpace.CreateGlobalJson(SDKVersion, "latestMinor", allowPrerelease);
+        var emptyBlazorServerApp1Dir = testFixtureSpace.GetTestAppProjDir("EmptyBlazorServerApp1");
+        CsprojTools.Rewrite(emptyBlazorServerApp1Dir, targetFramework, testFixtureSpace.BlazingStoryTargetsPath);
+
+        var dotnetVersion = await XProcess.Start("dotnet", "--version", emptyBlazorServerApp1Dir, TestHelper.XProcessOptions).WaitForExitAsync();
+        dotnetVersion.ExitCode.Is(0, message: dotnetVersion.Output);
+        dotnetVersion.Output.Is(ver => ver.StartsWith(SDKVersion.Substring(0, 4)), message: dotnetVersion.Output);
+
+        // When
+        using var dotnetPublish = XProcess.Start("dotnet", "publish -c Release", emptyBlazorServerApp1Dir, TestHelper.XProcessOptions);
+        await dotnetPublish.WaitForExitAsync();
+        dotnetPublish.ExitCode.Is(0, message: dotnetPublish.Output);
+
+        // Then
+        var publishDir = Path.Combine(emptyBlazorServerApp1Dir, "bin", "Release", targetFramework, "publish");
+        var xdocCommentForApp = XDocument.Load(uri: Path.Combine(publishDir, "EmptyBlazorServerApp1.xml"));
+        (xdocCommentForApp.Element("doc")?.Element("assembly")?.Element("name")?.Value).Is("EmptyBlazorServerApp1");
+
+        var xdocCommentForLib = XDocument.Load(uri: Path.Combine(publishDir, "RazorClassLib1.xml"));
+        (xdocCommentForLib.Element("doc")?.Element("assembly")?.Element("name")?.Value).Is("RazorClassLib1");
+
+        var xdocCommentForFramework = XDocument.Load(uri: Path.Combine(publishDir, "Microsoft.AspNetCore.Components.Web.xml"));
+        (xdocCommentForFramework.Element("doc")?.Element("assembly")?.Element("name")?.Value).Is("Microsoft.AspNetCore.Components.Web");
     }
 }
 #endif
