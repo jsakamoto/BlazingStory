cf33bb236282dd0ed8c3910c7a8f64676a5ae91a
jsakamoto <junichi.jp@gmail.com>
2025-05-04 22:24:17 +0900

    Implementing direct markdown file support

---
+++ BlazingStory/Internals/Pages/MarkdownPageBase.cs
@@ -0,0 +1,17 @@
+﻿using BlazingStory.Internals.Extensions;
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+
+namespace BlazingStory.Internals.Pages;
+
+public class MarkdownPageBase : ComponentBase
+{
+    [Inject]
+    public IJSRuntime JSRuntime { get; set; } = default!;
+
+    protected override async Task OnAfterRenderAsync(bool firstRender)
+    {
+        await using var prism = await this.JSRuntime.ImportAsync("prism.js");
+        await prism.InvokeVoidAsync("Prism.highlightAll");
+    }
+}
