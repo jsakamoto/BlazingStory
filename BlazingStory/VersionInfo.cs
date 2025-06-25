3a8794e789f610fb522e28c3569b3e20f549e8c0
jsakamoto <junichi.jp@gmail.com>
2025-06-26 08:28:24 +0900

    v.1.0.0-preview.54 release

--- BlazingStory/VersionInfo.cs
+++ BlazingStory/VersionInfo.cs
@@ -1,8 +1,8 @@
 namespace BlazingStory;
 internal static class VersionInfo
 {
-    internal static string GetVersionText() => "1.0.0-preview.53";
-    internal static string GetEscapedVersionText() => "1.0.0-preview.53";
-    internal static string GetFormattedVersionText() => "1.0 Preview 53";
+    internal static string GetVersionText() => "1.0.0-preview.54";
+    internal static string GetEscapedVersionText() => "1.0.0-preview.54";
+    internal static string GetFormattedVersionText() => "1.0 Preview 54";
     internal static string GetBuildTimestamp() => "June 2025";
 }
