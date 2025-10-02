ee869c7308efa5451d23017fe2652921d01508be
WimVdSElia <131674105+WimVdSElia@users.noreply.github.com>
2025-10-02 10:55:45 +0200

    chore(release): bump version to 1.0.0-preview.61 and update release notes

--- BlazingStory/VersionInfo.cs
+++ BlazingStory/VersionInfo.cs
@@ -1,8 +1,8 @@
 namespace BlazingStory;
 internal static class VersionInfo
 {
-    internal static string GetVersionText() => "1.0.0-preview.60";
-    internal static string GetEscapedVersionText() => "1.0.0-preview.60";
-    internal static string GetFormattedVersionText() => "1.0 Preview 60";
-    internal static string GetBuildTimestamp() => "September 2025";
+    internal static string GetVersionText() => "1.0.0-preview.61";
+    internal static string GetEscapedVersionText() => "1.0.0-preview.61";
+    internal static string GetFormattedVersionText() => "1.0 Preview 61";
+    internal static string GetBuildTimestamp() => "October 2025";
 }
