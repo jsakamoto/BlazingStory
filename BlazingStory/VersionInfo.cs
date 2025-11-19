9205f21f1e25a97afc792eff5b75f81773f67f32
jsakamoto <junichi.jp@gmail.com>
2025-11-19 11:56:22 +0900

    Bumped the package version

--- BlazingStory/VersionInfo.cs
+++ BlazingStory/VersionInfo.cs
@@ -1,8 +1,8 @@
 namespace BlazingStory;
 internal static class VersionInfo
 {
-    internal static string GetVersionText() => "1.0.0-preview.61";
-    internal static string GetEscapedVersionText() => "1.0.0-preview.61";
-    internal static string GetFormattedVersionText() => "1.0 Preview 61";
-    internal static string GetBuildTimestamp() => "October 2025";
+    internal static string GetVersionText() => "1.0.0-preview.62";
+    internal static string GetEscapedVersionText() => "1.0.0-preview.62";
+    internal static string GetFormattedVersionText() => "1.0 Preview 62";
+    internal static string GetBuildTimestamp() => "November 2025";
 }
