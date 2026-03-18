935b971b4f1f4b5dc498df74989191e5a315930d
jsakamoto <junichi.jp@gmail.com>
2026-03-19 08:53:37 +0900

    Bump version to 1.0.0-preview.68 and add Addon mechanism release notes

--- BlazingStory.Addons.BuiltIns/VersionInfo.cs
+++ BlazingStory.Addons.BuiltIns/VersionInfo.cs
@@ -1,12 +1,6 @@
 namespace BlazingStory.Addons.BuiltIns;
 
-/// <summary>
-/// Provides version information for the BlazingStory.Addons.BuiltIns assembly.
-/// </summary>
 internal static class VersionInfo
 {
-    /// <summary>
-    /// Returns the URL-escaped version text for cache-busting static assets.
-    /// </summary>
-    internal static string GetEscapedVersionText() => "1.0.0-preview.67";
+    internal static string GetEscapedVersionText() => "1.0.0-preview.68";
 }
--- BlazingStory/VersionInfo.cs
+++ BlazingStory/VersionInfo.cs
@@ -1,8 +1,8 @@
 namespace BlazingStory;
 internal static class VersionInfo
 {
-    internal static string GetVersionText() => "1.0.0-preview.67";
-    internal static string GetEscapedVersionText() => "1.0.0-preview.67";
-    internal static string GetFormattedVersionText() => "1.0 Preview 67";
+    internal static string GetVersionText() => "1.0.0-preview.68";
+    internal static string GetEscapedVersionText() => "1.0.0-preview.68";
+    internal static string GetFormattedVersionText() => "1.0 Preview 68";
     internal static string GetBuildTimestamp() => "March 2026";
 }
