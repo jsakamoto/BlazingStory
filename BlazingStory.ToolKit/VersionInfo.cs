e7afb3dc5642c316da3f6c12d8f9a2ae45cd7b41
jsakamoto <junichi.jp@gmail.com>
2026-03-28 11:46:40 +0900

    v.1.0.0-preview.71 release

--- BlazingStory.ToolKit/VersionInfo.cs
+++ BlazingStory.ToolKit/VersionInfo.cs
@@ -2,5 +2,5 @@ namespace BlazingStory.ToolKit;
 
 internal static class VersionInfo
 {
-    internal static string GetEscapedVersionText() => "1.0.0-preview.70";
+    internal static string GetEscapedVersionText() => "1.0.0-preview.71";
 }
