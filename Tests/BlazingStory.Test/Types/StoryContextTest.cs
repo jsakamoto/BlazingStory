c98ccbbfeb7c6618bf83e660974675ecf38998e4
WimVdSElia <131674105+WimVdSElia@users.noreply.github.com>
2026-06-12 15:11:50 +0200

    test(types): update story context parameter count

--- Tests/BlazingStory.Test/Types/StoryContextTest.cs
+++ Tests/BlazingStory.Test/Types/StoryContextTest.cs
@@ -18,6 +18,6 @@ internal class StoryContextTest
         var parameterCount = context.GetNoEventParameterCount();
 
         // Then
-        parameterCount.Is(5);
+        parameterCount.Is(6);
     }
 }
