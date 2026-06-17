b7139de3d6fa897a82b857dd467170dfc3281bac
jsakamoto <junichi.jp@gmail.com>
2026-06-17 09:02:17 +0900

    Fix ArrayParameterController rendering test to pass context via CascadingValue

--- Tests/BlazingStory.Test/Internals/Components/ParameterControllers/ArrayParameterControllerTest.cs
+++ Tests/BlazingStory.Test/Internals/Components/ParameterControllers/ArrayParameterControllerTest.cs
@@ -1,6 +1,8 @@
+using BlazingStory.Addons.BuiltIns.Panel.Controls.ParameterControllers;
 using BlazingStory.Addons.BuiltIns.Panel.Controls.ParameterControllers.Controllers;
 using BlazingStory.Test._Fixtures.Components;
 using Bunit;
+using Microsoft.AspNetCore.Components;
 
 namespace BlazingStory.Test.Internals.Components.ParameterControllers;
 
@@ -12,14 +14,14 @@ public class ArrayParameterControllerTest
         // Given
         using var ctx = new BunitContext();
         var parameter = SampleComponent.CreateComponentParameter(nameof(SampleComponent.Numbers));
+        var parameterContext = new ParameterControllerContext("numbers", parameter, new[] { 1, 2, 3 }, EventCallback.Factory.Create<ParameterInputEventArgs>(new(), _ => { }));
 
         // When
-        var cut = ctx.Render<ArrayParameterController>(builder => builder
-            .Add(_ => _.Key, "numbers")
-            .Add(_ => _.Parameter, parameter)
-            .Add(_ => _.Value, new[] { 1, 2 }));
+        var cut = ctx.Render<CascadingValue<ParameterControllerContext>>(parameters => parameters
+            .Add(p => p.Value, parameterContext)
+            .AddChildContent<ArrayParameterController>());
 
         // Then
-        cut.FindAll("input[type='number']").Count.Is(2);
+        cut.FindAll("input[type='number']").Count.Is(3);
     }
 }
